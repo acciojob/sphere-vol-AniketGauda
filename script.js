0996bc295dbfada31b6c7e277e95fd6e5d30ea0b
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-18 00:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+	e.preventDefault();
 	let r = document.getElementById("radius");
 
     let v = document.getElementById("volume");
